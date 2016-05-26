ec41b30adf0d99cce767ec5c80f8eb080eb8d7bd
Tim Colgan <tim@colgan.id.au>
2016-05-26 10:28:46 +1000

    message

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,41 @@
 ## functions do
 
 ## Write a short comment describing this function
+## makeCacheMatrix creates a matrix structure in the global environment space
+## to cache the inverse of a specific matrix. The inversion is performed once 
+## and subsequent requests for inversion will be retrieved from 
+## the cache
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
 
 ## Write a short comment describing this function
+## cacheSolve performs a matrix inversion and stores the result in the cache, or retrieves the previous inversion result. 
+## The outcome is returned
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
+
